95ee7296bc8f9a0c7bd7c1a31315416b7884cd17
Kenneth Jan Lopez <kennethjan.lopez@students.isatu.edu.ph>
2024-04-21 01:17:01 +0800

    Initial Commit

---
+++ Python_Workbook#4/personalModule.py
@@ -0,0 +1,13 @@
+"""
+This module is for personal use and serves as an experimental module
+
+Author: Kenneth Jan Lopez
+"""
+
+author = "Kenneth Jan Lopez"
+
+def displayAuthor():
+    return author
+
+def displayAge():
+    return 20
